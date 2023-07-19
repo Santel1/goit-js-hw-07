d110fc8a8d53359b3fcf98f63426d91bd2b92afd
Santel1 <126976920+Santel1@users.noreply.github.com>
2023-07-19 19:03:01 +0200

    hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = createGalleryCards(galleryItems);
+
+let instance;
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryList.addEventListener("click", onClickGalleryCard);
+
+function createGalleryCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </li>`;
+    })
+    .join("");
+}
+
+function onClickGalleryCard(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const originalSrc = evt.target.dataset.source;
+
+  instance = basicLightbox.create(`
+    <img src="${originalSrc}" width="800" height="600">
+`);
+
+  document.addEventListener("keydown", onModalClose);
+  instance.show();
+}
+
+function onModalClose(evt) {
+  if (evt.code === "Escape") {
+    document.removeEventListener("keydown", onModalClose);
+
+    instance.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = createGalleryCards(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryList.addEventListener("click", onClickGalleryCard);
+
+function createGalleryCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+    </li>`;
+    })
+    .join("");
+}
+
+function onClickGalleryCard(evt) {
+  evt.preventDefault();
+
+  const gallery = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
